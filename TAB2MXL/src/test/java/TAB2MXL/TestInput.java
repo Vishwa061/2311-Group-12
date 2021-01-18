e85fe0ebbeb0ae0ed589911c14d702e726c67342
Vishwa Perera <pereravishwa061@gmail.com>
2021-01-18 12:22:46 -0500

    Test Input

---
+++ TAB2MXL/src/test/java/TAB2MXL/TestInput.java
@@ -0,0 +1,26 @@
+package TAB2MXL;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestInput {
+	@Test
+	public void testReadFile() {
+		Main main = new Main();
+		
+		List<String> expected = new ArrayList<String>();
+		expected.add("e |-------------0-0-0-0-0-0-----0-------0-0-0-0-0---|");
+		expected.add("B |-------------1-1-1-1-1-1-1h3p1p0h1-----1-1-1-1-1-|");
+		expected.add("G |-----0h2-----2-2-2-2-2-2-----2-------2-2-2-2-2---|");
+		expected.add("D |-0h2-------2-2-2-2-2-2-2-----2-----2-2-2-2-2-2---|");
+		expected.add("A |---------0---0-0-0-0-0-----------0---0-0-0-0-0---|");
+		expected.add("E |-------------------------------------------------|");
+		
+		List<String> actual = main.readFile("test_tabs_reading.txt");
+		
+		assertEquals(expected, actual);
+	}
+
+}
