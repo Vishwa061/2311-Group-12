1d74704e4e8529551e4a3e6fb4c100ad8b6f58c5
Sara Araibi <sara012@my.yorku.ca>
2021-01-19 15:31:14 -0500

    Sara added a line in main

--- TAB2MXL/src/main/java/TAB2MXL/Main.java
+++ TAB2MXL/src/main/java/TAB2MXL/Main.java
@@ -16,6 +16,8 @@ public class Main {
 		
 		System.out.println("Nish made a change");
 		System.out.println("Savvy changed Java version to 1.8");
+		System.out.println("Sara made a change.");
+		
 		}
 	
 	public List<String> readFile(String fileLocation) {
