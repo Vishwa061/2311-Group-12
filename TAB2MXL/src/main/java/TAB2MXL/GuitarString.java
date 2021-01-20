5d090946abeebae8c7f725c80b8716fbcf426675
Vishwa Perera <pereravishwa061@gmail.com>
2021-01-19 19:49:06 -0500

    Added GuitarString

---
+++ TAB2MXL/src/main/java/TAB2MXL/GuitarString.java
@@ -0,0 +1,35 @@
+package TAB2MXL;
+
+class GuitarString {
+	private static final String[] ALL_NOTES = {"E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"};
+	private int tuningValue;
+	private int stringNumber;
+
+	public GuitarString(int stringNumber, String stringTuning) {
+		this.stringNumber = stringNumber;
+		for (int i = 0; i < ALL_NOTES.length; i++) {
+			if (stringTuning == ALL_NOTES[i]) {
+				tuningValue = i;
+				break;
+			}
+		}
+	}
+
+	public String getStep(int fret) {
+		int noteIndex = (tuningValue + fret) % ALL_NOTES.length;
+		String note = ALL_NOTES[noteIndex];
+		return Character.toString(note.charAt(0));
+	}
+	
+	public int getAlter(int fret) {
+		int noteIndex = (tuningValue + fret) % ALL_NOTES.length;
+		String note = ALL_NOTES[noteIndex];
+		return note.length() - 1;
+	}
+	
+	public int getOctave(int fret) {
+		// need to finish this
+		return 0;
+	}
+
+}
