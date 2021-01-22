6ca4b26a0a5d5360d8e3ddb33c79427b1a7a5bcb
Vishwa Perera <pereravishwa061@gmail.com>
2021-01-22 04:08:42 -0500

    Finished creating octave + Added createPitch method

--- TAB2MXL/src/main/java/TAB2MXL/GuitarString.java
+++ TAB2MXL/src/main/java/TAB2MXL/GuitarString.java
@@ -1,7 +1,11 @@
 package TAB2MXL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GuitarString {
-	private static final String[] ALL_NOTES = {"E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"};
+	private static final String[] ALL_NOTES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+	private static final Map<Integer, Integer> OPEN_GUITAR_OCTAVES = initGuitarOctaves();
 	private int tuningValue;
 	private int stringNumber;
 
@@ -14,22 +18,38 @@ public class GuitarString {
 			}
 		}
 	}
-
-	public String getStep(int fret) {
+	
+	/**
+	 * Creates and returns a Pitch object which has the step, alter, and octave
+	 * 
+	 * @param fret
+	 * @return a Pitch object
+	 */
+	public Pitch createPitch(int fret) {
 		int noteIndex = (tuningValue + fret) % ALL_NOTES.length;
 		String note = ALL_NOTES[noteIndex];
-		return Character.toString(note.charAt(0));
-	}
-	
-	public int getAlter(int fret) {
-		int noteIndex = (tuningValue + fret) % ALL_NOTES.length;
-		String note = ALL_NOTES[noteIndex];
-		return note.length() - 1;
-	}
-	
-	public int getOctave(int fret) {
-		// need to finish this
-		return 0;
+		
+		String step = Character.toString(note.charAt(0));
+		int alter = note.length() - 1;
+		int octave = OPEN_GUITAR_OCTAVES.get(stringNumber) + (tuningValue + fret) / ALL_NOTES.length;
+		
+		return new Pitch(step, alter, octave);
 	}
 
+	public int getStringNumber() {
+		return stringNumber;
+	}
+	
+	private static Map<Integer, Integer> initGuitarOctaves() {
+		Map<Integer, Integer> guitarOctaves = new HashMap<Integer, Integer>();
+		guitarOctaves.put(1, 4);
+		guitarOctaves.put(2, 3);
+		guitarOctaves.put(3, 3);
+		guitarOctaves.put(4, 3);
+		guitarOctaves.put(5, 2);
+		guitarOctaves.put(6, 2);
+		
+		return guitarOctaves;
+	}
+	
 }
