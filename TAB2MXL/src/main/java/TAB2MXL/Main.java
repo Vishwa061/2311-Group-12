4d09bc09c662bf061efa69579fbae218adba35e9
Nishiket-singh <nishiketsingh.0@gmail.com>
2021-01-27 13:16:58 -0500

    conflict

--- TAB2MXL/src/main/java/TAB2MXL/Main.java
+++ TAB2MXL/src/main/java/TAB2MXL/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 		System.out.println("Hello World Nishiket"); 
 
-		System.out.println("Hello World, kkaneez"); 
+		System.out.println("Hello World"); 
 		System.out.println("Hello World, kaneez");
 		System.out.println("Hello World, savvy"); 
 
