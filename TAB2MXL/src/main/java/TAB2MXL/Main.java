79848ca79210e90a2a8e67250e2f38c9da5532e4
Kaneez Fatima <kaneez24@my.yorku.ca>
2021-01-27 13:13:57 -0500

    local

--- TAB2MXL/src/main/java/TAB2MXL/Main.java
+++ TAB2MXL/src/main/java/TAB2MXL/Main.java
@@ -11,14 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
-		System.out.println("Hello World Nishiket"); 
-
-		System.out.println("Hello World, kkaneez"); 
-		System.out.println("Hello World, kaneez");
-		System.out.println("Hello World, savvy"); 
-
-
+		System.out.println("Hello AFTERNOON"); 
 	}
 
 	public List<String> readFile(String fileLocation) {
