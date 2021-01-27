85646cafb770f5f192e584e5b7afa510d774822f
sgill94 <sgill94@my.yorku.ca>
2021-01-27 12:14:01 -0500

    deleted all print statements in main

--- TAB2MXL/src/main/java/TAB2MXL/Main.java
+++ TAB2MXL/src/main/java/TAB2MXL/Main.java
@@ -10,23 +10,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World"); //sara
+		System.out.println("Hello World"); 
 
-		
-		System.out.println("Savvy added.");
-		System.out.println("Sara added to Java 1.8.");
-		System.out.println("Push test");
-
-		System.out.println("Nish made a change");
-		System.out.println("Savvy changed Java version to 1.8");
-		System.out.println("Sara made a change.");
-		
-		System.out.println("kaneez made a change.");
-		System.out.println("another change.");
-		// savvy lab 1
-		//sara made a change
-		// kaneez yes
-		//nish made a change
 	}
 
 	public List<String> readFile(String fileLocation) {
