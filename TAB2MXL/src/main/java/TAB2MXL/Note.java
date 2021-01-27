e60f0fdfaf21707ec269493cc9a0d5d40cb6dd87
sgill94 <sgill94@my.yorku.ca>
2021-01-27 12:07:27 -0500

    deleted note and renamed notev2 to note

--- TAB2MXL/src/main/java/TAB2MXL/Note.java
+++ TAB2MXL/src/main/java/TAB2MXL/Note.java
@@ -1,17 +1,26 @@
 package TAB2MXL;
 
-public class Note {
-	private Pitch p;
-	private Notations n;
-	
-	public Note(Pitch p, Notations n) {
-		this.p = p;
-		this.n = n;
-	}
-	
-	public String toMXL() {
-		// TODO
-		return null;
-	}
-}
 
+public class Note {
+	
+	public PitchV2 pitch;
+	//public NotationV2 notation; 
+	public int duration; 
+	public int type;
+	
+	Note(PitchV2 pitch){
+		this.pitch = pitch;
+	}
+	
+
+	public String toString() {
+		String toMXL = "<note>\r\n" + this.pitch.toString() 
+				+ "            <duration> method not complete </duration>\r\n"
+				+ "            <type> method not complete </type>\r\n"
+				+ "            <staff>1</staff>\r\n"
+				+ "            </note>";
+		
+		return toMXL;
+	}
+
+}
