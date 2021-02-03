237eeb1563375bcc5e658fa0297e27bb9f56f560
Kaneez Fatima <kaneez24@my.yorku.ca>
2021-02-02 20:43:55 -0500

    main frame basic

---
+++ TAB2MXL/src/main/java/application/MyApplication.java
@@ -0,0 +1,29 @@
+package application; 
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.Paint;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+
+public class MyApplication implements ActionListener {
+	
+	JButton SelectIcon;
+	JButton submit;
+	
+}
