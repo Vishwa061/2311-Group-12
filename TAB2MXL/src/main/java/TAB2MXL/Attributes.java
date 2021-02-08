f598bcf9af5cce81c9ca75c4a5c95223fa020de9
Vishwa Perera <pereravishwa061@gmail.com>
2021-02-08 13:46:58 -0500

    Added Attributes Functionality

--- TAB2MXL/src/main/java/TAB2MXL/Attributes.java
+++ TAB2MXL/src/main/java/TAB2MXL/Attributes.java
@@ -1,14 +1,43 @@
 package TAB2MXL;
 
-public class Attributes {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Attributes() {
-		// TODO Auto-generated constructor stub
+public class Attributes {
+	private List<String> guitarTuning = new ArrayList<String>();
+
+	public Attributes(List<String> guitarTuning) {
+		this.guitarTuning = guitarTuning;
 	}
-	
+
 	@Override
 	public String toString() {
-		// TODO
-		return null;
+		String mxl = "\t<attributes>"
+				+ "\n\t\t<divisions>1</divisions>"
+				+ "\n\t\t<key>"
+				+ "\n\t\t\t<fifths>0</fifths>"
+				+ "\n\t\t</key>"
+				+ "\n\t\t<clef>"
+				+ "\n\t\t\t<sign>TAB</sign>"
+				+ "\n\t\t\t<line>5</line>"
+				+ "\n\t\t</clef>"
+				+ "\n\t\t<staff-details>";
+
+		mxl += "\n\t\t\t<staff-lines>" + guitarTuning.size() + "</staff-lines>";
+		for (int i = 1; i <= guitarTuning.size(); i++) {
+			int index = (guitarTuning.size() - i);
+			mxl += "\n\t\t\t<staff-tuning line=\"" + i + "\">"
+					+ "\n\t\t\t\t<tuning-step>" 
+					+ new Note(index + 1, guitarTuning.get(index), 0, 0).getPitch().getStep() 
+					+ "</tuning-step>"
+					+ "\n\t\t\t\t<tuning-octave>" 
+					+ new Note(index + 1, guitarTuning.get(index), 0, 0).getPitch().getOctave() 
+					+ "</tuning-octave>"
+					+ "\n\t\t\t</staff-tuning>";
+		}
+		mxl += "\n\t\t</staff-details>"
+				+ "\n\t</attributes>";
+
+		return mxl;
 	}
 }
