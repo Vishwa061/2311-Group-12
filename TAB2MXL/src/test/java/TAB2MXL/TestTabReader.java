2c55886aeff04f6775f32fb8c46e781c3cd4e2d1
Sara Araibi <sara012@my.yorku.ca>
2021-02-09 22:51:04 -0500

    finished methods

--- TAB2MXL/src/test/java/TAB2MXL/TestTabReader.java
+++ TAB2MXL/src/test/java/TAB2MXL/TestTabReader.java
@@ -14,12 +14,12 @@ public class TestTabReader {
 		TabReader reader = new TabReader(new File(PATH + "test_tabs_reading.txt"));
 		
 		List<String> expected = new ArrayList<String>();
-		expected.add("e |-------------0-0-0-0-0-0-----0-------0-0-0-0-0---|");
-		expected.add("B |-------------1-1-1-1-1-1-1h3p1p0h1-----1-1-1-1-1-|");
-		expected.add("G |-----0h2-----2-2-2-2-2-2-----2-------2-2-2-2-2---|");
-		expected.add("D |-0h2-------2-2-2-2-2-2-2-----2-----2-2-2-2-2-2---|");
-		expected.add("A |---------0---0-0-0-0-0-----------0---0-0-0-0-0---|");
-		expected.add("E |-------------------------------------------------|");
+		expected.add("e|-------------0-0-0-0-0-0-----0-------0-0-0-0-0---|");
+		expected.add("B|-------------1-1-1-1-1-1-1h3p1p0h1-----1-1-1-1-1-|");
+		expected.add("G|-----0h2-----2-2-2-2-2-2-----2-------2-2-2-2-2---|");
+		expected.add("D|-0h2-------2-2-2-2-2-2-2-----2-----2-2-2-2-2-2---|");
+		expected.add("A|---------0---0-0-0-0-0-----------0---0-0-0-0-0---|");
+		expected.add("E|-------------------------------------------------|");
 		
 		List<String> actual = reader.readFile(new File(PATH + "test_tabs_reading.txt"));
 		
@@ -27,10 +27,28 @@ public class TestTabReader {
 	}
 	
 	@Test
-	public void test1() {
-		TabReader reader = new TabReader(new File(PATH + "test1.txt"));
-		//System.out.println(reader.parseTab());
-		System.out.println(reader.splitMeasure());
+	public void testSplitMeasure() {
+		TabReader reader = new TabReader(new File(PATH + "TestSplitMeasure.txt"));
+		List<ArrayList<String>> expected = new ArrayList<ArrayList<String>>();
+		ArrayList<String> expMeasure1 = new ArrayList<String>();
+		expMeasure1.add("--0-----------------------");
+		expMeasure1.add("------------------3-----5-");
+		expMeasure1.add("------------------3-------");
+		expMeasure1.add("------------------5-------");
+		expMeasure1.add("--------------------------");
+		expMeasure1.add("--------------------------");
+		expected.add(expMeasure1);
+		ArrayList<String> expMeasure2 = new ArrayList<String>();
+		expMeasure2.add("-------------------------");
+		expMeasure2.add("-2-----------------------");
+		expMeasure2.add("-2-----------------------");
+		expMeasure2.add("-2-----------------------");
+		expMeasure2.add("-0-----------------------");
+		expMeasure2.add("-------------------------");
+		expected.add(expMeasure2);
+		
+		List<ArrayList<String>> actual = reader.splitMeasure();
+		assertEquals(expected, actual);
 	}
 	
 	@Test
