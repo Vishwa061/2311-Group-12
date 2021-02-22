04785cce9934afbf9dba49e07f94c9db910adb70
Kaneez Fatima <kaneez24@my.yorku.ca>
2021-02-21 22:20:30 -0500

    Resolved the warning error at runtime

--- TAB2MXL/src/main/java/application/Controller.java
+++ TAB2MXL/src/main/java/application/Controller.java
@@ -26,9 +26,6 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -63,94 +60,94 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Controller {
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private TextArea textOutputAreaXML;
+	@FXML
+	private TextArea textOutputAreaXML;
 
-    @FXML
-    private TextArea textInputFileArea;//outputbox
+	@FXML
+	private TextArea textInputFileArea;//outputbox
 
-    @FXML
-    private Button select;
+	@FXML
+	private Button select;
 
-    @FXML
-    private Button convert;
+	@FXML
+	private Button convert;
 
-    @FXML
-    private Button save;
-    
-    TabReader outputXMLFile;
+	@FXML
+	private Button save;
 
-    private Window stage;
-    BufferedReader input;
-    StreamResult output;
-    
-    @FXML
-    private File file; //needed
+	TabReader outputXMLFile;
 
-    @FXML
-    void ConvertClicked(ActionEvent event) {
-    	if(convert.getText().equals("Convert") && checkTrue(file) == true) {
-    		System.out.println("yas");
-    		//reads the file provided thr
-    		TabReader reader = new TabReader(file);
- 	    	System.out.println(reader.toMXL());
- 	    	textInputFileArea.appendText(reader.toMXL());
- 	    	
+	private Window stage;
+	BufferedReader input;
+	StreamResult output;
+
+	@FXML
+	private File file; //needed
+
+	@FXML
+	void ConvertClicked(ActionEvent event) {
+		if(convert.getText().equals("Convert") && checkTrue(file) == true) {
+			System.out.println("yas");
+			//reads the file provided thr
+			TabReader reader = new TabReader(file);
+			System.out.println(reader.toMXL());
+			textInputFileArea.appendText(reader.toMXL());
+
 		}
 		else  {
 			FileChooser fileChooser = new FileChooser();
-			  fileChooser.setTitle("Save");
-			  FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("musicXML files (*.musicxml)", "*.musicxml");
-			  fileChooser.getExtensionFilters().add(extFilter);
-			  File savefile = fileChooser.showSaveDialog(convert.getScene().getWindow());
-			  System.out.println("Successfully wrote to the file.");
+			fileChooser.setTitle("Save");
+			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("musicXML files (*.musicxml)", "*.musicxml");
+			fileChooser.getExtensionFilters().add(extFilter);
+			File savefile = fileChooser.showSaveDialog(convert.getScene().getWindow());
+			System.out.println("Successfully wrote to the file.");
 		}
-    }
+	}
 
-    @FXML
-    void SaveClicked(ActionEvent event) {
-    	 try { 
-   		  FileChooser fileChooser = new FileChooser();
-             FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("musicXML files (*.musicxml)","*.musicxml");
-             fileChooser.getExtensionFilters().add(extFilter);
-             File file = fileChooser.showSaveDialog(stage);
-             if(file != null){
-             	FileWriter myWriter = new FileWriter(file);
-             	myWriter.write(textInputFileArea.getText());
-   				myWriter.close();
-             //	SaveFile(textInput.getText(), file);
-   				
-   				
-   				Alert errorAlert = new Alert(AlertType.INFORMATION); //creates a displayable error allert window 
-   				errorAlert.setHeaderText("File is saved. Happy making music!"); 
-   				errorAlert.showAndWait();
-             }
-            }
-             catch (IOException ex) {
-             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-             }
-    }
+	@FXML
+	void SaveClicked(ActionEvent event) {
+		try { 
+			FileChooser fileChooser = new FileChooser();
+			FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("musicXML files (*.musicxml)","*.musicxml");
+			fileChooser.getExtensionFilters().add(extFilter);
+			File file = fileChooser.showSaveDialog(stage);
+			if(file != null){
+				FileWriter myWriter = new FileWriter(file);
+				myWriter.write(textInputFileArea.getText());
+				myWriter.close();
+				//	SaveFile(textInput.getText(), file);
 
-    @FXML
-    void dragFile() {
-    	textOutputAreaXML.setOnDragOver(e -> { //e -> dictates action needed 
+
+				Alert errorAlert = new Alert(AlertType.INFORMATION); //creates a displayable error allert window 
+				errorAlert.setHeaderText("File is saved. Happy making music!"); 
+				errorAlert.showAndWait();
+			}
+		}
+		catch (IOException ex) {
+			Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	@FXML
+	void dragFile() {
+		textOutputAreaXML.setOnDragOver(e -> { //e -> dictates action needed 
 			Dragboard db = e.getDragboard(); 
 			if(db.hasFiles() && db.getFiles().size() == 1) {
-				
+
 				try {
 					Path path = FileSystems.getDefault().getPath(db.getFiles().get(0).getPath());
 					if(Files.probeContentType(path).equals("text/plain")) {
 						e.acceptTransferModes(TransferMode.COPY);//copy data 
 					}
-					
+
 				} catch (IOException e1) {
-					
+
 					e1.printStackTrace();
 				}
 			}
@@ -158,12 +155,12 @@ public class Controller {
 				e.consume();
 			}
 		});
-    	textOutputAreaXML.setOnDragDropped(e->{
+		textOutputAreaXML.setOnDragDropped(e->{
 			Dragboard db = e.getDragboard();
 			boolean success = false;
 			if(db.hasFiles()) {
 				success = true;
-				
+
 				for(File f : db.getFiles()) {
 					file = f;
 					textInputFileArea.clear();
@@ -177,25 +174,25 @@ public class Controller {
 		});
 
 	}
-    
 
-    @FXML
-    void select() {
-    	FileChooser fileChooser = new FileChooser(); //enables the user to select one or more files via a file explorer from the user's local computer
+
+	@FXML
+	void select() {
+		FileChooser fileChooser = new FileChooser(); //enables the user to select one or more files via a file explorer from the user's local computer
 		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt"); //chooses only txt file 
 		fileChooser.setInitialFileName("myfile.txt"); // sets the file name to download 
 		fileChooser.getExtensionFilters().add(extFilter);
 		file = fileChooser.showOpenDialog(select.getScene().getWindow()); 	
-		
+
 		if(!(file.length() == 0)) {
 			textInputFileArea.clear();
 			textOutputAreaXML.clear();
 			checkTrue(file);
-        }
+		}
 		else {
 			ErrorOutput(file);
 		}
-    }
+	}
 
 	private boolean checkTrue(File file) {
 		if(!(file.length() == 0)) {
@@ -205,52 +202,52 @@ public class Controller {
 		}
 		return false;
 	}
-	
-
-    private String readFile(File file){
-
-        StringBuilder stringBuffer = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(file));
-            String text;
-            while ((text = bufferedReader.readLine()) != null) {
-                stringBuffer.append(text);
-            }
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-
-        } finally {
-            try {
-                bufferedReader.close();
-            } catch (IOException ex) {
-            }
-        }
-
-        return stringBuffer.toString();
-}
 
 
-	
+	private String readFile(File file){
+
+		StringBuilder stringBuffer = new StringBuilder();
+		BufferedReader bufferedReader = null;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(file));
+			String text;
+			while ((text = bufferedReader.readLine()) != null) {
+				stringBuffer.append(text);
+			}
+
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IOException ex) {
+
+		} finally {
+			try {
+				bufferedReader.close();
+			} catch (IOException ex) {
+			}
+		}
+
+		return stringBuffer.toString();
+	}
+
+
+
 	private void ErrorOutput(File file) {
 		Alert errorAlert = new Alert(AlertType.ERROR); //creates a displayable error alert window 
 		errorAlert.setHeaderText("Input file is not valid. Please ensure your input file is not empty"); 
 		errorAlert.setContentText("Provide text file"); //Shows this stage and waits for it to be hidden (closed) before returning to the caller.
 		errorAlert.showAndWait();
 	}
-	
 
-	
-    @FXML
-    void initialize() {
-        assert textOutputAreaXML != null : "fx:id=\"textOutputAreaXML\" was not injected: check your FXML file 'Untitled'.";
-        assert textInputFileArea != null : "fx:id=\"textInputFileArea\" was not injected: check your FXML file 'Untitled'.";
-        assert select != null : "fx:id=\"submit\" was not injected: check your FXML file 'Untitled'.";
-        assert convert != null : "fx:id=\"convert\" was not injected: check your FXML file 'Untitled'.";
-        assert save != null : "fx:id=\"save\" was not injected: check your FXML file 'Untitled'.";
 
-    }
+
+	@FXML
+	void initialize() {
+		assert textOutputAreaXML != null : "fx:id=\"textOutputAreaXML\" was not injected: check your FXML file 'Untitled'.";
+		assert textInputFileArea != null : "fx:id=\"textInputFileArea\" was not injected: check your FXML file 'Untitled'.";
+		assert select != null : "fx:id=\"submit\" was not injected: check your FXML file 'Untitled'.";
+		assert convert != null : "fx:id=\"convert\" was not injected: check your FXML file 'Untitled'.";
+		assert save != null : "fx:id=\"save\" was not injected: check your FXML file 'Untitled'.";
+
+	}
 }
 
